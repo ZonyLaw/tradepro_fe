24634d9af1413b4cf7f651b112bbe42c45b5ea48
yuyizhong <sunny_law@hotmail.com>
2024-12-01 07:52:45 +0000

    add buy_sentiment to prices

--- src/screens/PriceScreen.js
+++ src/screens/PriceScreen.js
@@ -17,23 +17,34 @@ useEffect(()=>{
 
     return (
       <div>
-        <h1>Prices for Ticker ID: {prices.full_name}</h1>
+        <h1>Prices for Ticker ID: {prices?.full_name || "Unknown"}</h1>
         {loading ? (
           <p>Loading...</p>
         ) : error ? (
           <p>{error}</p>
         ) : prices && prices.prices ? ( // Check if prices and prices.prices are defined
-          <ul>
-            {prices.prices.map((price) => (
-              <li key={price.id}>
-             {/*    <p>Date: {new Date(price.date).toLocaleString()}</p> */}
-                <p>Open: {price.open}</p>
-                <p>Close: {price.close}</p>
-                <p>Volume: {price.volume}</p>
-                <p>Trade: {price.trade}</p>
-              </li>
-            ))}
-          </ul>
+          <table className="table-bordered-custom" >
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Open</th>
+                <th>Close</th>
+                <th>Sentiment</th>
+                <th>Trade</th>
+              </tr>
+            </thead>
+            <tbody>
+              {prices.prices.map((price) => (
+                <tr key={price.id}>
+                  <td>{new Date(price.date).toLocaleString()}</td>
+                  <td>{price.open.toFixed(2)}</td>
+                  <td>{price.close.toFixed(2)}</td>
+                  <td>{price.buy_sentiment.toFixed(0)}%</td>
+                  <td>{price.trade}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         ) : (
           <p>No prices available.</p> // Add a fallback message in case there are no prices
         )}
